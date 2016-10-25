3d13bfc77475c087cb79452409606023d02b40a9
xjc <2585441871@qq.com>
2016-10-25 10:33:06 +0800

    账户管理首页跳转

--- src/pages/accountManagement/home/index.js
+++ src/pages/accountManagement/home/index.js
@@ -1,14 +1,30 @@
 // react 相关库
 import React from 'react';
+import {Link} from 'react-router';
 
 // antd 组件
-import { Alert } from 'antd';
+import {Alert} from 'antd';
 
 // 页面
 export default class Home extends React.Component {
-  render() {
-    return (
-      <div>首页</div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                首页
+
+                <p>
+                    <Link to="/accountManagement/home">账户管理首页</Link>
+                </p>
+                <p>
+                    <Link to="/accountManagement/basicInformation">账户管理基本信息</Link>
+                </p>
+                <p>
+                    <Link to="/accountManagement/resetPassword">账户管理修改密码</Link>
+                </p>
+                <p>
+                    <Link to="/accountManagement/resetTradingPassword">账户管理修改交易密码</Link>
+                </p>
+            </div>
+        );
+    }
 }
