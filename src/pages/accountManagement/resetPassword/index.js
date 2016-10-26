22284904028637940c5822bdc605b1012d12967d
Ethan <17881055@qq.com>
2016-10-26 17:38:25 +0800

    修改登录密码页面修改

--- src/pages/accountManagement/resetPassword/index.js
+++ src/pages/accountManagement/resetPassword/index.js
@@ -2,14 +2,45 @@
 import React from 'react';
 // antd 组件
 import { Button, Form, Input } from 'antd';
+// 自定义验证 rule
+import ruleType from 'UTILS/ruleType';
 // 页面
 import Frame from 'COM/form/frame';
-export default class extends React.Component {
+class ResetPassword extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    componentDidMount() {
+    }
+
     handleSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFields((errors, values) => {
+            if (!!errors) {
+                console.log('Errors in form!!!');
+                return;
+            }
+            console.log('Submit!!!');
+            console.log(values);
+        });
+    }
+
+    checkPass(rule, value, callback) {
+        const { validateFields } = this.props.form;
+        if(value) {
+            validateFields(['confirmPassword'], {force: true});
+        }
+        callback();
+    }
+
+    checkConfirmPass(rule, value, callback) {
+        const { getFieldValue } = this.props.form;
+        if(value && value !== getFieldValue('password')) {
+            callback('两次输入密码不一致！');
+        } else {
+            callback();
+        }
     }
 
     render() {
@@ -17,25 +48,53 @@ export default class extends React.Component {
             labelCol: {span: 8},
             wrapperCol: {span: 8},
         };
+        const {getFieldProps} = this.props.form; //用于和表单进行双向绑定
+        const oldPasswdProps = getFieldProps(' oldPassword', { //原登录密码
+            rules: [
+                {required: true, whitespace: true, message: '请输入原登录密码'},
+            ],
+        });
+        const passwdProps = getFieldProps('password', {//新登录密码
+            rules: [
+                {required: true, min: 8, max: 20, message: '请输入8-20个字符'},
+                {validator: this.checkPass.bind(this)},
+                ruleType('en-num')
+            ],
+        });
+        const confirmPasswordProps = getFieldProps('confirmPassword', {//确认新登录密码
+            rules: [
+                {required: true, message: '请再次输入密码'},
+                {validator: this.checkConfirmPass.bind(this)},
+            ],
+        });
         return (
             <Frame title="修改登录密码" small="  避免使用有规律的数字或字母，请勿与交易密码一致。">
                 <div className="fn-pt-30">
-                    <Form horizontal>
+                    <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
                         <Form.Item {...props} label="登录名">
                             <label>HYP123</label>
                         </Form.Item>
-                        <Form.Item {...props} label="原登录密码" required>
-                            <Input type="password"/>
+                        <Form.Item {...props} label="原登录密码" hasFeedback required>
+                            <Input type="password"
+                                   autoComplete="off"
+                                   placeholder=""
+                                {...oldPasswdProps}/>
                         </Form.Item>
-                        <Form.Item {...props} label="新登录密码" required>
-                            <Input type="password" autoComplete="off" placeholder="8-20位英文字母（区分大小写）、数字或符号的组合"/>
+                        <Form.Item {...props} label="新登录密码" hasFeedback required>
+                            <Input type="password"
+                                   autoComplete="off"
+                                   placeholder="8-20位英文字母（区分大小写）、数字或符号的组合"
+                                {...passwdProps}/>
                         </Form.Item>
-                        <Form.Item {...props} label="确认新登录密码" required>
-                            <Input type="password" autoComplete="off" placeholder="8-20位英文字母（区分大小写）、数字或符号的组合"/>
+                        <Form.Item {...props} label="确认新登录密码" hasFeedback required>
+                            <Input type="password"
+                                   autoComplete="off"
+                                   placeholder="8-20位英文字母（区分大小写）、数字或符号的组合"
+                                {...confirmPasswordProps} />
                         </Form.Item>
-                        <Form.Item wrapperCol={{ span: 12, offset: 8 }}>
-                            <Button type="primary" onClick={this.handleSubmit}>确认提交</Button>
-                            <a href="#">取消</a>
+                        <Form.Item wrapperCol={{span: 12, offset: 8}}>
+                            <Button type="primary" htmlType="submit">确认提交</Button>
+                            <a style={{paddingLeft: 20}} href="#">取消</a>
                         </Form.Item>
                     </Form>
                 </div>
@@ -43,3 +102,4 @@ export default class extends React.Component {
         )
     }
 }
+export default Form.create()(ResetPassword)
