0706645384c3ec389f9fb2ac7764baf1f6e8d612
xjc <2585441871@qq.com>
2016-10-27 18:00:52 +0800

    更新

--- src/pages/accountManagement/resetTradingPassword/Steps1/RealNameAuthentication.js
+++ src/pages/accountManagement/resetTradingPassword/Steps1/RealNameAuthentication.js
@@ -20,7 +20,9 @@ const Step = Steps.Step;
 const FormItem = Form.Item;
 
 import codeimg from 'ASSETS/images/code.png';
-import { IdentityModal, SupplementModal } from 'BCOM/Modal/index';
+import {IdentityModal, SupplementModal} from 'BCOM/Modal/index';
+
+
 
 // 页面身份验证
 export default class RealNameAuthentication extends React.Component {
@@ -29,8 +31,8 @@ export default class RealNameAuthentication extends React.Component {
         this.state = {
             visible: false,
             data: this.props.data,
-            isValidation:this.props.isValidation,
-    		identityVisible : false,
+            isValidation: this.props.isValidation,
+            identityVisible: false
         }
 
     }
@@ -55,35 +57,29 @@ export default class RealNameAuthentication extends React.Component {
         this.setState({visible: false});
     }
     showIdentityModal() {
-	    this.setState({
-	      identityVisible: true,
-	    });
-	    console.log(this);
-	}
+        this.setState({identityVisible: true});
+        console.log(this);
+    }
 
-	closeIdentityModal() {
-	    this.setState({
-	      identityVisible: false,
-	    });
-	    console.log(this);
-	}
+    closeIdentityModal() {
+        this.setState({identityVisible: false});
+        console.log(this);
+    }
 
+    handleMessage() {
+
+        fetch('/api/home/getLastLoginTime').then(res => res.json()).then(res => {
+            alert(JSON.stringify(res));
+        })
+    }
 
     render() {
         return (
             <div>
-                <Modal
-                    title="钱途互联提示"
-                    visible={this.state.visible}
-                    onOk={this.handleOk.bind(this)}
-                    onCancel={this.handleCancel.bind(this)}
-                    okText="朕前去完善资料"
-                    cancelText="妾身再考虑一下"
-                    wrapClassName="vertical-center-modal"
-                    >
+                <Modal title="钱途互联提示" visible={this.state.visible} onOk={this.handleOk.bind(this)} onCancel={this.handleCancel.bind(this)} okText="朕前去完善资料" cancelText="妾身再考虑一下" wrapClassName="vertical-center-modal">
                     <p>
                         <span className="ant-exclamation-circle">
-                            <Icon type="exclamation-circle" />
+                            <Icon type="exclamation-circle"/>
                         </span>
                         您的实名认证未完成，请尽快完成。
                     </p>
@@ -93,12 +89,12 @@ export default class RealNameAuthentication extends React.Component {
                         <Col span={12} offset={6}>
                             <p>
                                 姓名：{this.state.data.realName}，您的身份识别码已发送到手机133****1234。
-                                <Link to='/'>没有收到短信，重新发送</Link>
+                                <a href='javascript:;' onClick={this.handleMessage.bind(this)}>没有收到短信，重新发送</a>
                             </p>
                             <p>
                                 请下载实名认证APP完成认证。
                                 <Button type="primary" onClick={this.showIdentityModal.bind(this)}>查看详细操作步骤</Button>
-                                <IdentityModal visible={ this.state.identityVisible } closeCallBack={ this.closeIdentityModal.bind(this) }/>
+                                <IdentityModal visible={this.state.identityVisible} closeCallBack={this.closeIdentityModal.bind(this)}/>
                             </p>
                         </Col>
                     </Row>
