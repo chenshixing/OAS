b4cb4d51ad203e5fc0151d0e7a1c0810f3107058
limit <minyi.lin@frontpay.cn>
2016-11-09 09:34:19 +0800

    update:dev环境配置

---
+++ config/webpack.devSever.js
@@ -0,0 +1,40 @@
+/**
+ * 连开发环境构建配置
+ */
+const path = require('path');
+const webpack = require('webpack');
+// 公用配置
+const commonConfig = require('./webpack.common');
+// WDS
+const utils = require('./utils');
+const HOST = utils.getIP();
+const PORT = 8090;
+
+module.exports = Object.assign(commonConfig, {
+    devtool: 'cheap-source-map', // 'eval'
+    cache: true,
+    plugins: commonConfig.plugins.concat([
+        // 配置全局常量
+        new webpack.DefinePlugin({
+            'process.env': { // React/Redux打包常用
+                NODE_ENV: JSON.stringify('development')
+            },
+            __DEV__: true,
+            __WHY_DID_YOU_UPDATE__: false // 是否检测不必要的组件重渲染
+        })
+    ]),
+    // webpack dev server 配置
+    devServer: {
+        host: HOST,
+        port: PORT,
+        proxy: {
+            '/api/*': {
+                target: 'http://accountcmb.frontpay.cn/',
+                pathRewrite: {
+                    '^/api': ''
+                },
+                secure: false
+            }
+        }
+    }
+});
