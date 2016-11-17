5e20e5ae8d2a5af044895e12fb667a5fe9fa5630
koen <440241969@qq.com>
2016-11-17 11:38:13 +0800

    修改头部信息

--- src/pages/Layouts/Common/header.js
+++ src/pages/Layouts/Common/header.js
@@ -16,20 +16,18 @@ export default class Header extends React.Component{
         return true;
     }
     render() {
-      let showName = '';
-      if(this.props.dat){
-        showName = this.props.data.data;
-      }
+      const { showName } = this.props.state.data;
+      const { logoutUrl } = this.props.state.sysInfo;
       return (
         <div className="the-main-nav-wrap">
           <div className="the-main-nav-box">
-            <a href="首页.html" className="logo-wrap">
+            <a href="/" className="logo-wrap">
 
             </a>
             <div className="login-box ">
-              {showName? <a href="javascript:;void(0)" title="当前登录用户">您好，{showName}</a> : null}
-              <a href="javascript:;void(0)" title="首页">首页</a>
-              {showName? <a href="javascript:;void(0)" title="退出">退出</a> : null}
+              {showName? <a title={`当前登录用户${showName}`}>您好，{showName}</a> : null}
+              <a href="/" title="首页">首页</a>
+              {showName? <a href={logoutUrl} title="退出">退出</a> : null}
             </div>
           </div>
         </div>
