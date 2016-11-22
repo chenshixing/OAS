86761fcb048e8adf7b741c33f0e523ee128996ad
limit <minyi.lin@frontpay.cn>
2016-11-22 18:49:27 +0800

    update:邮箱长度限制报错提示文案修改

--- src/pages/companyValidate/components/formValidation.js
+++ src/pages/companyValidate/components/formValidation.js
@@ -2,91 +2,132 @@
 import ruleType from 'UTILS/ruleType';
 
 const rulesBase = {
-    companyName : {
-        rules:[
-            {required: true, message: '企业名称不能为空'},
-            {max: 50, message: '请输入50位以内的字符'},
+    companyName: {
+        rules: [{
+            required: true,
+            message: '企业名称不能为空'
+        }, {
+            max: 50,
+            message: '请输入50位以内的字符'
+        }, ]
+    },
+    registrationExtendField2: {
+        rules: [{
+                required: true,
+                type: 'object',
+                message: '营业执照到期日不能为空'
+            },
+            ruleType('futureTime', '非法的营业执照日期'),
         ]
     },
-    registrationExtendField2:{
-        rules:[
-            {required: true, type: 'object', message: '营业执照到期日不能为空'},
-            ruleType('futureTime','非法的营业执照日期'),
-        ]
+    name: {
+        rules: [{
+            required: true,
+            message: '您的姓名不能为空'
+        }, {
+            max: 50,
+            message: '请输入50位以内的字符'
+        }, ]
     },
-    name:{
-        rules:[
-            {required: true, message: '您的姓名不能为空'},
-            {max: 50, message: '请输入50位以内的字符'},
-        ]
-    },
-    mobile :{
-        rules:[
-            {required: true, type: 'string', message: '填写人常用手机号码不能为空'},
+    mobile: {
+        rules: [{
+                required: true,
+                type: 'string',
+                message: '填写人常用手机号码不能为空'
+            },
             ruleType('mobile')
         ]
     },
     email: {
         rules: [
-            ruleType('email'),
-            {max: 50, message: '请输入99位以内的字符'},
+            ruleType('email'), {
+                max: 50,
+                message: '请输入50位以内的字符'
+            },
         ]
     },
-    cardNo:{
-        rules:[
-            {required: true, message: '银行账号不能为空'},
-            {min:5, max: 30, message: '请输入5-30位字符'},
-        ]
+    cardNo: {
+        rules: [{
+            required: true,
+            message: '银行账号不能为空'
+        }, {
+            min: 5,
+            max: 30,
+            message: '请输入5-30位字符'
+        }, ]
     },
-    bankId:{
-        rules:[
-            {required: true, message: '开户行不能为空'},
-        ]
+    bankId: {
+        rules: [{
+            required: true,
+            message: '开户行不能为空'
+        }, ]
     },
-    provinceId:{
-        rules: [{ required: true, message: '所在省份不能为空'}],
+    provinceId: {
+        rules: [{
+            required: true,
+            message: '所在省份不能为空'
+        }],
     },
-    cityId:{
-        rules: [{ required: true, message: '所在城市不能为空'}],
+    cityId: {
+        rules: [{
+            required: true,
+            message: '所在城市不能为空'
+        }],
     },
-    branchBankId:{
-        rules:[
-            {required: true, message: '分支行不能为空'},
-        ]
+    branchBankId: {
+        rules: [{
+            required: true,
+            message: '分支行不能为空'
+        }, ]
     }
 }
 const rulesCommon = {
     registrationPaperNo: {
-        rules: [
-            {required: true, type: 'string', message: '营业执照注册号不能为空'},
-            {max: 50, message: '请输入50位以内的字符'},
-        ]
+        rules: [{
+            required: true,
+            type: 'string',
+            message: '营业执照注册号不能为空'
+        }, {
+            max: 50,
+            message: '请输入50位以内的字符'
+        }, ]
     },
-    orgInsCodePaperNo:{
-        rules:[
-            {required: true, type: 'string', message: '组织机构代码证不能为空'},
-            {max: 50, message: '请输入50位以内的字符'},
-        ]
+    orgInsCodePaperNo: {
+        rules: [{
+            required: true,
+            type: 'string',
+            message: '组织机构代码证不能为空'
+        }, {
+            max: 50,
+            message: '请输入50位以内的字符'
+        }, ]
     }
 };
 
 const rulesMultiple = {
     socialCreditPaperNo: {
-        rules: [
-            {required: true, type: 'string', message: '统一社会信用代码不能为空'},
-            {max: 50, message: '请输入50位以内的字符'},
-        ]
+        rules: [{
+            required: true,
+            type: 'string',
+            message: '统一社会信用代码不能为空'
+        }, {
+            max: 50,
+            message: '请输入50位以内的字符'
+        }, ]
     },
 };
 
 const rulesAgent = {
     corporationName: {
-        rules: [
-            {required: true, message: '法定代表人姓名不能为空'},
-            {max: 50, message: '请输入50位以内的字符'},
-        ]
+        rules: [{
+            required: true,
+            message: '法定代表人姓名不能为空'
+        }, {
+            max: 50,
+            message: '请输入50位以内的字符'
+        }, ]
     },
-    corporationMobile : {
+    corporationMobile: {
         rules: [
             ruleType('mobile')
         ]
@@ -99,8 +140,8 @@ const rulesAgent = {
 };
 
 export default {
-	rulesBase : rulesBase,
-	rulesCommon : rulesCommon,
-	rulesMultiple : rulesMultiple,
-	rulesAgent : rulesAgent,
+    rulesBase: rulesBase,
+    rulesCommon: rulesCommon,
+    rulesMultiple: rulesMultiple,
+    rulesAgent: rulesAgent,
 }
