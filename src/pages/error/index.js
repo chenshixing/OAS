de46ec28e04ca969e71a177d1ff3976ac46853ba
koen <440241969@qq.com>
2016-11-23 18:01:14 +0800

    修改错误信息

--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -6,8 +6,9 @@ import ReactDOM from 'react-dom';
 
 export default class errPage extends React.Component {
   render() {
+    const msg = this.props.location.query.p || '无';
     return (
-      <div>错误相关信息</div>
+      <div>错误信息：{msg}</div>
     );
   }
 }
