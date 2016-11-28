1259ec8bcdecbedbcb890847b50dab93f0cecbc2
koen <440241969@qq.com>
2016-11-28 16:51:33 +0800

    修复state兼容IE8问题

--- src/pages/Layouts/state.js
+++ src/pages/Layouts/state.js
@@ -1,11 +1,15 @@
 /**
  * for Layouts
+ * 
+ * by koen
+ * 2016/11/16
  */
-import { restate } from 'UTILS';
+//import { restate } from 'UTILS';
+import restate from 'UTILS/restate';// 不写上面那句因为有些复杂依赖情况下babel会找不到方法导致出错
 
-class State {
-    constructor() {
-        this.state = {
+// 不写ES6的Class形式是因为压缩代码后在IE8会报错，具体原因无法考究
+function State(){
+    this.state = {
             loading: false, // 全局ajax的loading状态
             sysInfo: { // 全局用到的业务系统信息
                 "logoutUrl": "",
@@ -14,7 +18,6 @@ class State {
             }
         };
         this.sessionId = 'layouts_state';
-    }
 }
 
-export default restate.createState(State)
+export default restate.createState(State);
--- src/pages/redirect/state.js
+++ src/pages/redirect/state.js
@@ -5,11 +5,12 @@
  * by koen
  * 2016/11/16
  */
-import { restate } from 'UTILS';
+//import { restate } from 'UTILS';
+import restate from 'UTILS/restate';// 不写上面那句因为有些复杂依赖情况下babel会找不到方法导致出错
 
-class State {
-    constructor () {
-        // 初始化的state数据
+// 不写ES6的Class形式是因为压缩代码后在IE8会报错，具体原因无法考究
+function State() {
+    // 初始化的state数据
         this.state = {
             data: {
                 "userType": 0, // 用户类型，1：个人；2：企业
@@ -25,7 +26,6 @@ class State {
             }
         };
         this.sessionId = 'redirect_state';
-    }
 }
 
-export default restate.createState(State)
+export default restate.createState(State);
